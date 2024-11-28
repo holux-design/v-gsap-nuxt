4ba48a71fc682dbbe9c8d6a7812e3b8890902af0
holux-design <office@holux-design.at>
2024-11-28 22:51:35 +0100

    clean up playground

--- playground/app.vue
+++ playground/app.vue
@@ -1,20 +1,5 @@
 <template>
-  <div
-    style="
-      height: 300vh;
-      width: 100vw;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: center;
-      gap: 200px;
-    "
-  >
-    <div
-      v-gsap.whenVisible.from="{ x: -100, opacity: 0 }"
-      style="width: 100px; height: 100px; background: red"
-    />
-  </div>
+  <pre>Hello World</pre>
 </template>
 
 <script setup></script>
