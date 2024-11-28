3d6033270acb2ace7128b072eed61727119adc99
Lukas Hofstätter <lukas@five-am.com>
2024-11-28 12:02:14 +0100

    initial commit

---
+++ playground/app.vue
@@ -0,0 +1,20 @@
+<template>
+  <div
+    style="
+      height: 300vh;
+      width: 100vw;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      gap: 200px;
+    "
+  >
+    <div
+      style="width: 100px; height: 100px; background: red"
+      v-gsap.whenVisible.from="{ x: -100, opacity: 0 }"
+    ></div>
+  </div>
+</template>
+
+<script setup></script>
