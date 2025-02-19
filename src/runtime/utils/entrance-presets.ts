abbc7537e05ab8d4de42f885328a8b0dce4b9f7c
holux-design <office@holux-design.at>
2025-02-19 19:18:29 +0100

    fix(entrance): use `fromTo` instead of `from` to fix reverse playing on upscroll

--- src/runtime/utils/entrance-presets.ts
+++ src/runtime/utils/entrance-presets.ts
@@ -1,37 +1,37 @@
 export const entrancePresets = [
   {
     name: 'slide-left',
-    modifiers: 'whenVisible.fromInvisible.once.from',
-    value: { x: -32 },
+    modifiers: 'whenVisible.fromInvisible.once.fromTo',
+    value: [{ x: -32 }, { x: 0 }],
   },
   {
     name: 'slide-right',
-    modifiers: 'whenVisible.fromInvisible.once.from',
-    value: { x: 32 },
+    modifiers: 'whenVisible.fromInvisible.once.fromTo',
+    value: [{ x: 32 }, { x: 0 }],
   },
   {
     name: 'slide-top',
-    modifiers: 'whenVisible.fromInvisible.once.from',
-    value: { y: -32 },
+    modifiers: 'whenVisible.fromInvisible.once.fromTo',
+    value: [{ y: -32 }, { y: 0 }],
   },
   {
     name: 'slide-bottom',
-    modifiers: 'whenVisible.fromInvisible.once.from',
-    value: { y: 32 },
+    modifiers: 'whenVisible.fromInvisible.once.fromTo',
+    value: [{ y: 32 }, { y: 0 }],
   },
   {
     name: 'scale',
-    modifiers: 'whenVisible.fromInvisible.once.from',
-    value: { scale: 0.75 },
+    modifiers: 'whenVisible.fromInvisible.once.fromTo',
+    value: [{ scale: 0.75 }, { scale: 1 }],
   },
   {
     name: 'scale-full',
-    modifiers: 'whenVisible.fromInvisible.once.from',
-    value: { scale: 0 },
+    modifiers: 'whenVisible.fromInvisible.once.fromTo',
+    value: [{ scale: 0 }, { scale: 1 }],
   },
   {
     name: 'fade',
-    modifiers: 'whenVisible.fromInvisible.once.to',
-    value: { autoAlpha: 1 },
+    modifiers: 'whenVisible.fromInvisible.once.fromTo',
+    value: [{ autoAlpha: 0 }, { autoAlpha: 1 }],
   },
 ]
